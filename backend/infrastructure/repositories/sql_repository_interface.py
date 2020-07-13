68c210b696834c0fb727a7e843f1d2987f2205b5
Sergi Oliveres <solive@uic.es>
2020-07-13 08:51:30 +0200

    app models implements Model Interface

--- backend/infrastructure/repositories/sql_repository_interface.py
+++ backend/infrastructure/repositories/sql_repository_interface.py
@@ -1,15 +1,18 @@
+from backend.domain.model import Model
+
+
 class SQLRepositoryInterface:
     def fetch_all(self):
         pass
 
-    def fetch_by_id(self, id: int):
+    def fetch_one(self, id: int):
         pass
 
-    def create(self, id: int):
+    def create(self, model: Model):
         pass
 
-    def update(self, id: int):
+    def update(self, model: Model):
         pass
 
-    def destroy(self, id: int):
+    def destroy(self, model: Model):
         pass
